c0a6ae1bf73773f989afda217dbb3df9ed028c0f
nguyenhoangnam6199 <Nguyennam96747@gmail.com>
2020-11-19 21:58:55 +0700

    thêm label cho form bảng kê

--- QLVT_DATHANG/formBangKe.Designer.cs
+++ QLVT_DATHANG/formBangKe.Designer.cs
@@ -36,6 +36,7 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.lb_TIEUDE = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -126,11 +127,23 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // lb_TIEUDE
+            // 
+            this.lb_TIEUDE.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lb_TIEUDE.AutoSize = true;
+            this.lb_TIEUDE.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_TIEUDE.Location = new System.Drawing.Point(140, 29);
+            this.lb_TIEUDE.Name = "lb_TIEUDE";
+            this.lb_TIEUDE.Size = new System.Drawing.Size(519, 32);
+            this.lb_TIEUDE.TabIndex = 13;
+            this.lb_TIEUDE.Text = "CHI TIẾT TRỊ GIÁ HÀNG NHẬP /XUẤT";
+            // 
             // formBangKe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_TIEUDE);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
@@ -157,5 +170,6 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lb_TIEUDE;
     }
 }
